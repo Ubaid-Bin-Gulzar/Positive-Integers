1e01bb3eeecfea52e3a50a1269fb6ab5a30e3fed
Ubaidbingulzar <77198878+Ubaidbingulzar@users.noreply.github.com>
2021-01-11 10:29:07 +0530

    Create numbers.py
    This will Print positive numbers in a list of numbers.

---
+++ numbers.py
@@ -0,0 +1,15 @@
+# Python program to print positive Numbers in a List 
+  
+#This is a list of numbers 
+Input = [10, -11, 25, 0, -35] 
+  
+# by iterating each number in a list 
+for num in Input: 
+      
+    # checking condition 
+    if num >= 0: 
+       print(num, end = " ") 
+       
+# The output of numbers will be like this
+Output= 10 25 0
+       
